d09de51c6cecc62a34f65424f18f56cb74efef2d
Anomgames <101978954+Anomgames@users.noreply.github.com>
2026-02-12 18:11:52 +0300

    Add initial video data to wwevideolar.js

---
+++ wwevıdeolar.js
@@ -0,0 +1,22 @@
+[
+  {
+    "id": "I-4-SkzBUiY",
+    "baslik": "Damian Priest vs. Kit Wilson (WWE Main Event 2025)"
+  },
+  {
+    "id": "Angp_PrNPRk", 
+    "baslik": "LA Knight vs. Drew McIntyre vs. Jimmy Uso (SmackDown 2025)"
+  },
+  {
+    "id": "hT3PSURpwCg", 
+    "baslik": "Randy Orton Highlights"
+  },
+  {
+    "id": "oKRl_vhSvx8", 
+    "baslik": "Elimination Chamber 2025"
+  },
+  {
+    "id": "5a8eVa6QZk8", 
+    "baslik": "Seth Rollins vs Logan Paul"
+  }
+]
